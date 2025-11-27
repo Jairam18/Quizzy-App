88a28afca28b21b4ed1549110d0eceb457bee15d
Jairam18 <163737628+Jairam18@users.noreply.github.com>
2025-11-28 00:07:09 +0530

    Update App.jsx

--- quizzy/src/App.jsx
+++ quizzy/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./Component/home.jsx";
 import Login from "./Component/login.jsx";
 import Signup from "./Component/signup.jsx";
@@ -11,6 +12,7 @@ import ContactUs from "./Component/contactus.jsx";
 import Pricing from "./Component/Pricing.jsx"; 
 import Blog from "./Component/Blog.jsx";
 import HelpGuide from "./Component/HelpGuide.jsx";
+
 const App = () => {
   const [exams, setExams] = useState([]);
 
@@ -19,32 +21,22 @@ const App = () => {
   };
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/lms" element={<Lms />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/quiz" element={<Quiz />} />
+      <Route path="/lms" element={<Lms />} />
+      <Route path="/builder" element={<ExamBuilder onAddExam={addExam} />} />
+      <Route path="/live" element={<Live exams={exams} />} />
+      <Route path="/contact" element={<ContactUs />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/helpguide" element={<HelpGuide />} />
 
-        {/* Exam Builder alone */}
-        <Route path="/builder" element={<ExamBuilder onAddExam={addExam} />} />
-
-        {/* Live page shows all exams */}
-        <Route path="/live" element={<Live exams={exams} />} />
-
-        {/* Contact Us page */}
-        <Route path="/contact" element={<ContactUs />} />
-
-        {/* Pricing Page */}
-        <Route path="/pricing" element={<Pricing />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/helpguide" element={<HelpGuide />} />
-
-
-        <Route path="*" element={<h1>404 Page Not Found</h1>} />
-      </Routes>
-    </Router>
+      {/* 404 fallback */}
+      <Route path="*" element={<h1>404 Page Not Found</h1>} />
+    </Routes>
   );
 };
 
